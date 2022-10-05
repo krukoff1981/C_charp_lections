de43f99da512b4813b565fda55683580c05e0102
krukoff1981 <krukoff1981@yandex.ru>
2022-10-06 00:34:29 +0300

    вторая лекция - массивы и методы

---
+++ Lesson2/00101/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+int n = array.Length;
+int i = 0;
+int index = 0;
+while (index < n)
+{
+   if(array[index]%2==0)
+   {
+        i++;
+   } 
+    index++;
+}
+Console.WriteLine(i);
---
+++ Lesson2/00110/Program.cs
@@ -0,0 +1,48 @@
+﻿void FillArray(int[] collection) //метод вставка чисел в массив
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 100);
+
+        index++;
+    }
+}
+
+void PrintArray(int[] coll) // метод вывод массива на печать
+{
+    int count = coll.Length;
+    int position = 0;
+    while (position < count)
+    {
+    Console.WriteLine(coll[position]);
+    position++;
+    }
+}
+
+int IndexOf(int[]collect, int find) // метод поиск конкретного значения
+{
+    int count = collect.Length;
+    int index = 0;
+    int position = -1;
+    while(index<count)
+    {
+        if(collect[index]==find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[20];
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 9);
+Console.WriteLine(pos);
